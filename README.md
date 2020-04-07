be5cc95617fdf27f735420d95c6792e0caf7a075
dominikbelter <dominik.belter@gmail.com>
2020-04-07 15:42:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ompl_example_2d
+
+# Dependencies:
+
+$ sudo apt-get install ros-melodic-map-server ros-melodic-dwa-local-planner libompl-dev
+
+# Installation
+
+$ cd ~/catkin_ws/src
+$ git clone https://github.com/dominikbelter/ompl_example_2d
+$ cd ~/catkin_ws/
+$ catkin_make
+
+# Run example
+
+$ roslaunch ompl_example_2d ompl_example_2d.launch
+
+# Tasks
+
+1. Use subscribed occupancy map to define forbiden states for the robot
+2. Compare RRT-Connect algorithm with othes available planners
---
+++ include/ompl_example_2d/ompl_example_2d.hpp
@@ -0,0 +1,99 @@
+/*
+ * ompl_example_2d.hpp
+ *
+ *  Created on: April 6, 2020
+ *      Author: Dominik Belter
+ *	 Institute: Instute of Robotics and Machine Intelligence, Poznan University of Technology
+ */
+
+#pragma once
+
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+// ROS
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Path.h>
+#include <nav_msgs/OccupancyGrid.h>
+
+#include <moveit/ompl_interface/ompl_interface.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+
+// Boost
+#include <boost/thread.hpp>
+
+// standard
+#include <mutex>
+#include <iostream>
+#include <thread>
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+
+namespace ompl_example_2d {
+
+/*!
+ * 2D planner example class
+ */
+class Planner2D
+{
+public:
+
+    /*!
+   * Constructor.
+   * @param nodeHandle the ROS node handle.
+   */
+    Planner2D(ros::NodeHandle& _nodeHandle);
+
+    /*!
+   * Destructor.
+   */
+    virtual ~Planner2D();
+
+    /*!
+   * plan path
+   */
+    nav_msgs::Path planPath(const nav_msgs::OccupancyGrid& globalMap);
+
+private:
+    /// node handle
+    ros::NodeHandle& nodeHandle;
+
+    /// occupancy map used for planning
+    nav_msgs::OccupancyGrid occupancyMap;
+
+    /// problem dim
+    int dim;
+
+    /// max step length
+    double maxStepLength;
+
+    /// bounds for the x axis
+    std::shared_ptr<ompl::base::RealVectorBounds> coordXBound;
+
+    /// bounds for the y axis
+    std::shared_ptr<ompl::base::RealVectorBounds> coordYBound;
+
+    /// start position
+    std::shared_ptr<ompl::base::ScopedState<>> start;
+
+    /// goal position
+    std::shared_ptr<ompl::base::ScopedState<>> goal;
+
+    /// search space
+    std::shared_ptr<ompl::base::StateSpace> space;
+
+    /// configure node
+    void configure(void);
+
+    /// check if the current state is valid
+    static bool isStateValid(const ompl::base::State *state);
+
+    /// extract path
+    nav_msgs::Path extractPath(ompl::base::ProblemDefinition* pdef);
+};
+
+} /* namespace */
