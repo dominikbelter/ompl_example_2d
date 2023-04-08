0c1c5464631687aea3b0a5e359533e5a17e8d8e8
Dominik Belter <dominik.belter@gmail.com>
2023-04-08 09:49:57 +0200

    next step - ros2 implementation

---
+++ include/ompl_example_2d/ompl_example_2d.hpp
@@ -0,0 +1,90 @@
+/*
+ * ompl_example_2d.hpp
+ *
+ *  Created on: April 6, 2020
+ *  Updated on: April 8, 2023
+ *      Author: Dominik Belter
+ *	 Institute: Instute of Robotics and Machine Intelligence, Poznan University of Technology
+ */
+
+#pragma once
+
+// ROS
+#include <ros/ros.h>
+#include "geometry_msgs/msg/pose_stamped.h"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+
+// #include <moveit/ompl_interface/ompl_interface.h>
+#include <ompl-1.6/ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl-1.6/ompl/base/spaces/RealVectorStateSpace.h>
+
+// Boost
+#include <boost/thread.hpp>
+
+// standard
+#include <mutex>
+#include <iostream>
+#include <thread>
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+
+namespace ompl_example_2d {
+
+/*!
+ * 2D planner example class
+ */
+class Planner2D
+{
+public:
+
+    /*!
+   * Constructor.
+   * @param nodeHandle the ROS node handle.
+   */
+    Planner2D(ros::NodeHandle& _nodeHandle);
+
+    /*!
+   * Destructor.
+   */
+    virtual ~Planner2D();
+
+    /*!
+   * plan path
+   */
+    nav_msgs::msg::Path planPath(const nav_msgs::msg::OccupancyGrid& globalMap);
+
+private:
+    /// node handle
+    ros::NodeHandle& nodeHandle;
+
+    /// problem dim
+    int dim;
+
+    /// max step length
+    double maxStepLength;
+
+    /// bounds for the x axis
+    std::shared_ptr<ompl::base::RealVectorBounds> coordXBound;
+
+    /// bounds for the y axis
+    std::shared_ptr<ompl::base::RealVectorBounds> coordYBound;
+
+    /// start position
+    std::shared_ptr<ompl::base::ScopedState<>> start;
+
+    /// goal position
+    std::shared_ptr<ompl::base::ScopedState<>> goal;
+
+    /// search space
+    std::shared_ptr<ompl::base::StateSpace> space;
+
+    /// configure node
+    void configure(void);
+
+    /// extract path
+    nav_msgs::msg::Path extractPath(ompl::base::ProblemDefinition* pdef);
+};
+
+} /* namespace */
--- src/ompl_example_2d_node.cpp
+++ src/ompl_example_2d_node.cpp
@@ -1,10 +1,62 @@
-#include <cstdio>
+#include <memory>
+#include <chrono>
+#include <functional>
+#include <string>
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include <nav_msgs/msg/path.hpp> 
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+nav_msgs::msg::OccupancyGrid globalMap;
+
+class MapSubscriber : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+  public:
+    MapSubscriber()
+    : Node("map_subscriber")
+    {
+      subscription_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+      "/map", 10, std::bind(&MapSubscriber::map_callback, this, _1));
+    }
 
-  printf("hello world ompl_example_2d package\n");
+  private:
+    void map_callback(const nav_msgs::msg::OccupancyGrid & msg) const
+    {
+      globalMap = msg;
+    }
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr subscription_;
+};
+
+class PathPublisher : public rclcpp::Node
+{
+  public:
+    PathPublisher()
+    : Node("path_publisher")
+    {
+      publisher_ = this->create_publisher<nav_msgs::msg::Path>("/planned_path", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&PathPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = nav_msgs::msg::Path();
+      // message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: ");
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MapSubscriber>());
+  rclcpp::shutdown();
   return 0;
 }
