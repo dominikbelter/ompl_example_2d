edd7d339eedf7cb3262e77dc0fa321b73940bb05
Bartłomiej Kulecki <b.kulecki@gmail.com>
2023-05-02 18:34:31 +0200

    remove example_maps deps

--- launch/ompl_example_2d.launch.py
+++ launch/ompl_example_2d.launch.py
@@ -1,22 +1,18 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
-from launch.conditions import IfCondition
 
 
 def generate_launch_description():
 
-    backtrace = LaunchConfiguration('backtrace', default='False')
-
-    map_path = os.path.join(get_package_share_directory('example_maps'), 'maps', 'map_home.yaml')
+    map_path = os.path.join(get_package_share_directory('ompl_example_2d'), 'maps', 'map_home.yaml')
 
     map_server_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('example_maps'), 'launch', 'map_launch.py')
+            os.path.join(get_package_share_directory('ompl_example_2d'), 'launch', 'map_launch.py')
         ),
         launch_arguments={'map': map_path}.items()
     )
